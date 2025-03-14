a74c0e1e8ea02ba7f23d852bf19dce004038fcdb
christophemr <christophemr59@gmail.com>
2025-03-14 11:11:31 +0100

    header file of the project

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
+
+#endif /* SEARCH_ALGOS_H */
