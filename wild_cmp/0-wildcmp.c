03472e3efc7689ec05775157b0104e973c5ad82b
christophemr <christophemr59@gmail.com>
2025-03-21 17:10:36 +0100

    function that compares two strings and returns 1 if the strings can be considered identical

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Compares two strings, allowing '*' as a wildcard.
+ * @s1: The first string.
+ * @s2: The second string (can contain '*').
+ *
+ * Return: 1 if the strings are identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+    if (*s1 == '\0' && *s2 == '\0')
+        return (1);
+
+    if (*s2 == '*')
+    {
+        if (*(s2 + 1) == '\0')
+            return (1);
+        if (*s1 == '\0')
+            return (wildcmp(s1, s2 + 1));
+        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+    }
+
+    if (*s1 == *s2)
+        return (wildcmp(s1 + 1, s2 + 1));
+
+    return (0);
+}
